c44b6cc449231a01e69c78f1c34216d6e128f10a
tamilanban <anbantamizh@gmail.com>
2022-11-07 12:27:15 +1030

    first commit

---
+++ ImportCSVRazorSample/Models/Batting.cs
@@ -0,0 +1,11 @@
+﻿namespace ImportCSVRazorSample.Models
+{
+    public class Batting
+    {
+        public string FullName { get; set; }
+        public int Runs { get; set; }
+        public int BallsFaced { get; set; }
+        public string Venue { get; set; }
+
+    }
+}
---
+++ ImportCSVRazorSample/Pages/Battings.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using CsvHelper;
+using ImportCSVRazorSample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+using System.IO;
+
+namespace ImportCSVRazorSample.Pages
+{
+    public class BattingModel : PageModel
+    {
+        private readonly ILogger<BattingModel> _logger;
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment _environment;
+
+
+        [BindProperty]
+        public IFormFile UploadCSV { get; set; }
+
+        public IEnumerable<Batting> BattingStats { get; set; }
+
+        public BattingModel(ILogger<BattingModel> logger,
+            Microsoft.AspNetCore.Hosting.IHostingEnvironment environment)
+        {
+            _logger = logger;
+            _environment = environment;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            using (var stream = UploadCSV.OpenReadStream())
+            { 
+                using (var textStream = new StreamReader(stream))
+                {
+                    using (var csv = new CsvReader(textStream, CultureInfo.InvariantCulture))
+                    {
+                        BattingStats = csv.GetRecords<Batting>();
+                    }
+                }
+                return Page();
+            }
+        }
+    }
+}
